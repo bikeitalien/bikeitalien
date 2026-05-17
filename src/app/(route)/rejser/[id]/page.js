d8991c7619eb22680fd0a2ea898397e08d61a5d3
klhe0002 <klhe0002@stud.kea.dk>
2026-05-17 10:05:49 +0200

    Testimonials virker, men skal justeres i mobil og knapperne skal fikses

--- src/app/(route)/rejser/[id]/page.js
+++ src/app/(route)/rejser/[id]/page.js
@@ -2,6 +2,7 @@ import { supabase } from "@/lib/supabase";
 import Footer from "@/app/components/Footer";
 import ImageCardSection from "@/app/components/ImageCardSection";
 import ContactSection from "@/app/components/ContactSection";
+import Testimonials from "@/app/components/Testimonials";
 
 export default async function RejseDetalje({ params }) {
   const { id } = await params;
@@ -21,11 +22,12 @@ export default async function RejseDetalje({ params }) {
   return (
     <>
       <main className="py-32">
-        <h1>{rejse.titel}</h1>
+        {/* <h1>{rejse.titel}</h1>
         <p>{rejse.land}</p>
         <p>{rejse.antal_dage} dage</p>
-        <p>{rejse.pris} kr.</p>
+        <p>{rejse.pris} kr.</p> */}
         <ImageCardSection rejse={rejse} />
+        <Testimonials testimonials={rejse.testimonials.testimonials} />
         <ContactSection />
       </main>
       <footer className="col-[full] grid grid-cols-subgrid">
