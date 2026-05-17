04f0834539da8d39716fcc13e1a3e6dd5d6b7b93
klhe0002 <klhe0002@stud.kea.dk>
2026-05-17 12:28:51 +0200

    små ændringer på card

--- src/app/(route)/rejser/page.js
+++ src/app/(route)/rejser/page.js
@@ -88,7 +88,7 @@ async function TravelCardContainer({ searchParams }) {
 
   return (
     <section className="col-[content] grid py-10">
-      <div className="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-3">
+      <div className="grid grid-cols-1 gap-8 items-stretch md:grid-cols-2 xl:grid-cols-3">
         {travels.map((rejse) => (
           <TravelCard key={rejse.id} rejse={rejse} />
         ))}
