0d7123808720c75243e1e1230550479f41e8478d
Heleneomdahl <Heom0001@stud.kea.dk>
2026-05-17 10:44:00 +0200

    Add HeadingSection component to enhance ContactSection layout

--- src/app/components/HeadingSection.jsx
+++ src/app/components/HeadingSection.jsx
@@ -1,8 +1,14 @@
-const HeadingSection = () => {
+const HeadingSection = ({ tagline, title, children, className = "" }) => {
   return (
-    <>
-      <div></div>
-    </>
+    <div className={className}>
+      {tagline && (
+        <p className="text-(length:--tag-size)! font-medium">{tagline}</p>
+      )}
+      <div className="flex flex-col gap-4">
+        <h3>{title}</h3>
+        {children}
+      </div>
+    </div>
   );
 };
 
